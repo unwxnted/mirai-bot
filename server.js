db48cd936cd85da1f70132f3b81f4542dbff11a3
F4STZ <bauti0926@gmail.com>
2021-11-26 04:54:12 -0300

    updated dependencies

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const {
 	prefix,
